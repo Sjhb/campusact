9e71d407a37e97b2ac256d5adc23559ad54e7615
Sjhb <1402204813@qq.com>
2017-03-21 17:41:19 +0800

    修改mapper

--- src/main/webapp/browser/script/controllers/user/loginCtrl.js
+++ src/main/webapp/browser/script/controllers/user/loginCtrl.js
@@ -13,7 +13,11 @@
         $scope.changed=function () {
             $scope.worning='';
         }
-
+        var token = $("meta[name='_csrf']").attr("content");
+        var header = $("meta[name='_csrf_header']").attr("content");
+        $(document).ajaxSend(function(e, xhr, options) {
+            xhr.setRequestHeader(header, token);
+        });
         $scope.check={isEmpty:false,
                     check:function () {
                             this.isEmpty=false;
--- src/main/webapp/browser/script/core/constants.js
+++ src/main/webapp/browser/script/core/constants.js
@@ -3,7 +3,7 @@
  */
 (function(){
 	angular.module("activities").constant('env',{
-		activities_api:'/qike/',
+		activities_api:'/campusact/',
 		status_404:'404',
 		status_500:'500',
 		status_300:'300'
